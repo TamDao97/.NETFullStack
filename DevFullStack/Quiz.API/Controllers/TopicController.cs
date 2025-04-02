c5aaba8b52507a8057f973b3994d8ba9617e9af4
tamdao <tam.dao@bnksolution.com>
2025-04-02 07:30:21 +0700

    update document

---
+++ DevFullStack/Quiz.API/Controllers/TopicController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Quiz.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TopicController : ControllerBase
+    {
+        private readonly ILogger<TopicController> _logger;
+
+        public TopicController(ILogger<TopicController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("create")]
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+
+        }
+    }
+}
--- DevFullStack/Quiz.API/obj/Debug/net8.0/Quiz.API.AssemblyInfo.cs
+++ DevFullStack/Quiz.API/obj/Debug/net8.0/Quiz.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quiz.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c15cb806bd148644fe0a32bb86cbbb596e60d26b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b859ccf3082e95bb6201467289a7fd71ea8991a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quiz.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quiz.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
